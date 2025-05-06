1c9865ae95df0a49798419c7ad08fbb7dcb4e136
Максим Мигунов <migunovmaksim44@gmail.com>
2025-05-06 17:15:50 +0300

    fix main import processor

--- main.go
+++ main.go
@@ -1,9 +1,9 @@
 package main
 
 import (
-	"github.com/Killazius/yadro/Processor"
 	"github.com/Killazius/yadro/config"
 	"github.com/Killazius/yadro/event"
+	"github.com/Killazius/yadro/processor"
 	"os"
 )
 
